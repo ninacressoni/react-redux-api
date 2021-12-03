0f982917c1986c0688f6be5971bd5c2ae8cd2fae
Nina Cressoni <ninacressoni@gmail.com>
2021-12-03 13:23:25 -0300

    binding in classes

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,15 +1,11 @@
 import React from "react";
 
 class CoursePage extends React.Component {
-  constructor(props) {
-    super(props); 
-
-    this.state = {
-      course: {
-        title: ""
-      }
-    };
-  }
+  state = {
+    course: {
+      title: ""
+    }
+  };
 
 handleChange(event) {
   const course = { ...this.state.course, title: event.target.value};
@@ -21,7 +17,7 @@ handleChange(event) {
       <form>
         <h2>Courses</h2>
         <h3>Add Course</h3>
-        <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+        <input type="text" onChange={this.handleChange.bind(this)} value={this.state.course.title} />
         <input type="submit" value="Save" />
       </form>
     );
