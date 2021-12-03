4fecdfc63969655a34afe6dd77ed34abf6b238a1
Nina Cressoni <ninacressoni@gmail.com>
2021-12-03 15:20:05 -0300

    handling loading courses in reducer

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -5,14 +5,14 @@ export function createCourse(course) {
     return { type: types.CREATE_COURSE, course };
 }
 
-export function loadCourseSuccess(courses) {
-    return {type: types.LOAD_COURSES_SUCCESS, courses};
+export function loadCourseSuccess(course) {
+    return {type: types.LOAD_COURSES_SUCCESS, course};
 }
 
 export function loadCourses() {
     return function (dispatch) {
         return courseApi.getCourses().then(course => {
-            dispatch(loadCourseSuccess(courses));
+            dispatch(loadCourseSuccess(course));
         }).catch(error => {
             throw error;
         })
