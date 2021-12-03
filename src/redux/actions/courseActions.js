6c90d69fbb23b768b6d7643f6df37a95df6665bb
Nina Cressoni <ninacressoni@gmail.com>
2021-12-03 17:02:50 -0300

    dispatch create and update

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,20 +1,43 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-    return { type: types.CREATE_COURSE, course };
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
-export function loadCourseSuccess(course) {
-    return {type: types.LOAD_COURSES_SUCCESS, course};
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
-    return function (dispatch) {
-        return courseApi.getCourses().then(course => {
-            dispatch(loadCourseSuccess(course));
-        }).catch(error => {
-            throw error;
-        })
-    }
+  return function(dispatch) {
+    return courseApi
+      .getCourses()
+      .then(courses => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  //eslint-disable-next-line no-unused-vars
+  return function(dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
